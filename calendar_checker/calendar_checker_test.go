9b2a0c472b54db4860331d87f1726818f0d48918
Chiara Mezzavilla <faerie_queene87@yahoo.it>
2018-03-11 16:02:06 +0100

    next-avail Initial test for dates calculator

--- calendar_checker/calendar_checker_test.go
+++ calendar_checker/calendar_checker_test.go
@@ -1,21 +1,45 @@
 package calendar_checker
 
 import (
-	"fmt"
 	"testing"
 	"time"
+	"google.golang.org/api/calendar/v3"
 )
 
 func TestGetNextThreeEvenings(t *testing.T) {
 
-	afternoon_start, err := time.Parse(time.RFC3339, "2018-02-15T15:04:05+01:00")
+	// fixture
+	afternoon_start, _ := time.Parse(time.RFC3339, "2018-02-15T15:04:05+01:00")
 
-	if err == nil {
-		fmt.Println(GetNextThreeEvenings(afternoon_start))
-	} else {
-		fmt.Println(err)
+	first_slot := calendar.TimePeriod{
+		Start: "2018-02-15T16:04:05+01:00",
+		End: "2018-02-15T17:04:05+01:00",
 	}
 
+	second_slot := calendar.TimePeriod{
+		Start: "2018-02-16T18:04:05+01:00",
+		End: "2018-02-16T21:04:05+01:00",
+	}
 
+	calendar_example := calendar.FreeBusyCalendar{Busy:[]*calendar.TimePeriod{&first_slot, &second_slot}}
+
+	free_dates := make([]time.Time, 3)
+	free_dates[0], _ = time.Parse(time.RFC3339, "2018-02-15T19:00:00+01:00")
+	free_dates[1], _ = time.Parse(time.RFC3339, "2018-02-17T19:00:00+01:00")
+	free_dates[2], _ = time.Parse(time.RFC3339, "2018-02-18T19:00:00+01:00")
+
+	// execution
+	result := GetNextThreeEvenings(afternoon_start, calendar_example.Busy)
+
+
+	// check
+	for i, want := range(free_dates) {
+		
+		got := result[i]
+
+		if want != got {
+			t.Errorf("For date n. %v, wanted %v, got %v", i, want, got)
+		}
+	}
 
 }
