a1fe1a4e2b9c787bda14e21810f2757bc96593b2
Chiara Mezzavilla <samurang87+apps@gmail.com>
2018-03-19 20:39:06 +0100

    next-avail Add more tests and refactor variables
    One edge case is red

--- calendar_checker/calendar_checker_test.go
+++ calendar_checker/calendar_checker_test.go
@@ -6,30 +6,93 @@ import (
 	"google.golang.org/api/calendar/v3"
 )
 
-func TestGetNextThreeEvenings(t *testing.T) {
+func TestGetNextThreeEveningsFromAfternoon(t *testing.T) {
 
 	// fixture
-	afternoon_start, _ := time.Parse(time.RFC3339, "2018-02-15T15:04:05+01:00")
+	afternoonStart, _ := time.Parse(time.RFC3339, "2018-02-15T15:04:05+01:00")
 
-	first_slot := calendar.TimePeriod{
+	firstSlot := calendar.TimePeriod{
 		Start: "2018-02-15T16:04:05+01:00",
 		End: "2018-02-15T17:04:05+01:00",
 	}
 
-	second_slot := calendar.TimePeriod{
+	secondSlot := calendar.TimePeriod{
 		Start: "2018-02-16T18:04:05+01:00",
 		End: "2018-02-16T21:04:05+01:00",
 	}
 
-	calendar_example := calendar.FreeBusyCalendar{Busy:[]*calendar.TimePeriod{&first_slot, &second_slot}}
+	calendarExample := calendar.FreeBusyCalendar{Busy:[]*calendar.TimePeriod{&firstSlot, &secondSlot}}
 
-	free_dates := make([]time.Time, 3)
-	free_dates[0], _ = time.Parse(time.RFC3339, "2018-02-15T19:00:00+01:00")
-	free_dates[1], _ = time.Parse(time.RFC3339, "2018-02-17T19:00:00+01:00")
-	free_dates[2], _ = time.Parse(time.RFC3339, "2018-02-18T19:00:00+01:00")
+	freeDates := make([]time.Time, 3)
+	freeDates[0], _ = time.Parse(time.RFC3339, "2018-02-15T19:00:00+01:00")
+	freeDates[1], _ = time.Parse(time.RFC3339, "2018-02-17T19:00:00+01:00")
+	freeDates[2], _ = time.Parse(time.RFC3339, "2018-02-18T19:00:00+01:00")
 
 	// execution
-	result := GetNextThreeEvenings(afternoon_start, calendar_example.Busy)
+	result := GetNextThreeEvenings(afternoonStart, calendarExample.Busy)
+
+
+	// check
+	for i, want := range freeDates {
+
+		got := result[i]
+
+		if want != got {
+			t.Errorf("For date n. %v, wanted %v, got %v", i, want, got)
+		}
+	}
+
+}
+
+func TestGetNextThreeEveningsFromEvening(t *testing.T) {
+
+	// fixture
+	afternoonStart, _ := time.Parse(time.RFC3339, "2018-02-15T20:04:05+01:00")
+
+	onlySlot := calendar.TimePeriod{
+		Start: "2018-02-16T18:04:05+01:00",
+		End: "2018-02-16T21:04:05+01:00",
+	}
+
+	calendarExample := calendar.FreeBusyCalendar{Busy:[]*calendar.TimePeriod{&onlySlot}}
+
+	freeDates := make([]time.Time, 3)
+	freeDates[0], _ = time.Parse(time.RFC3339, "2018-02-17T19:00:00+01:00")
+	freeDates[1], _ = time.Parse(time.RFC3339, "2018-02-18T19:00:00+01:00")
+	freeDates[2], _ = time.Parse(time.RFC3339, "2018-02-19T19:00:00+01:00")
+
+	// execution
+	result := GetNextThreeEvenings(afternoonStart, calendarExample.Busy)
+
+
+	// check
+	for i, want := range freeDates {
+
+		got := result[i]
+
+		if want != got {
+			t.Errorf("For date n. %v, wanted %v, got %v", i, want, got)
+		}
+	}
+
+}
+
+func TestGetNextThreeEveningsButYouAreOnVacation (t *testing.T) {
+
+	// fixture
+	afternoonStart, _ := time.Parse(time.RFC3339, "2018-02-15T20:04:05+01:00")
+
+	onlySlot := calendar.TimePeriod{
+		Start: "2018-02-16T18:04:05+01:00",
+		End: "2018-02-2816T21:04:05+01:00",
+	}
+
+	calendar_example := calendar.FreeBusyCalendar{Busy:[]*calendar.TimePeriod{&onlySlot}}
+
+	free_dates := make([]time.Time, 3)
+
+	// execution
+	result := GetNextThreeEvenings(afternoonStart, calendar_example.Busy)
 
 
 	// check
