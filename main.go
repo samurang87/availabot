468cbd434cc03fc8ec7e89d107ce04ca58313e9b
Chiara Mezzavilla <samurang87+apps@gmail.com>
2018-03-20 20:53:03 +0100

    next-avail Simple example usage in main method

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"github.com/samurang87/availabot/calendar_checker"
 	"time"
+	"fmt"
 )
 
 func main() {
 
 	_, busy := calendar_checker.GetBusyCalendar(time.Now())
 
-	for _, slot := range busy {
+	threeFree := calendar_checker.GetNextThreeEvenings(time.Now(), busy)
 
-		fmt.Println(slot.Start)
-		fmt.Println(slot.End)
+	fmt.Println(threeFree)
 
-	}
 }
